10398bbc012f099f1644250d158295de896e9fa4
JUSTIVE <ben399399@naver.com>
2026-02-19 23:54:54 +0900

    feat: added icon, link wasn't string all the time

--- content.js
+++ content.js
@@ -6,7 +6,13 @@ const getTweetLink = (article) => {
 };
 
 const addLink = (article, link) => {
-  const iconsvg = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-square-quote-icon lucide-message-square-quote"><path d="M14 14a2 2 0 0 0 2-2V8h-2"/><path d="M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2z"/><path d="M8 14a2 2 0 0 0 2-2V8H8"/></svg>`;
+  const iconsvg = `<svg width="18.75" height="18.75" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M1.75101 10C1.75101 5.58 5.33501 2 9.75601 2H14.122C18.612 2 22.251 5.64 22.251 10.13C22.251 13.09 20.644 15.81 18.055 17.24L10.001 21.7V18.01H9.93401C5.44401 18.11 1.75101 14.5 1.75101 10ZM9.75601 4C6.43901 4 3.75101 6.69 3.75101 10C3.75101 13.37 6.52101 16.08 9.88901 16.01L10.24 16H12.001V18.3L17.088 15.49C19.039 14.41 20.251 12.36 20.251 10.13C20.251 6.74 17.507 4 14.122 4H9.75601Z" fill="currentColor"/>
+  <path d="M8 8.57143C8 7.15114 9.00725 6 10.25 6V7.71429C9.836 7.71429 9.5 8.09829 9.5 8.57143V9.42857H11V12H8V8.57143Z" fill="currentColor"/>
+  <path d="M13 8.57143C13 7.15114 14.0073 6 15.25 6V7.71429C14.836 7.71429 14.5 8.09829 14.5 8.57143V9.42857H16V12H13V8.57143Z" fill="currentColor"/>
+  </svg>
+
+`;
 
   const container = document.createElement("div");
   container.classList.add("chirpviz-icon-container");
@@ -46,7 +52,7 @@ const collectTweets = () => {
     if (tweet.querySelector(".chirpviz-anchor") === null) {
       const link = getTweetLink(tweet);
       console.log(link);
-      addLink(tweet, link.replaceAll("/photo/1", ""));
+      if (typeof link === "string") addLink(tweet, link.replaceAll("/photo/1", ""));
     }
   });
 };
